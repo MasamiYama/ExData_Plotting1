e3a2db4f718a6eeb62fa497236a759007c0dba24
MasamiYama <56776765+MasamiYama@users.noreply.github.com>
2019-12-28 11:09:55 +1000

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -36,5 +36,6 @@ png("plot3.png", width=480, height=480)
 plot(data$Date_Time, data$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering", col = "black")
 lines(data$Date_Time, data$Sub_metering_2, col = "red")
 lines(data$Date_Time, data$Sub_metering_3, col = "blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col =c("black", "red", "blue"), lty=1)
 dev.off()
 
